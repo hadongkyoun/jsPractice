1a04dc25520471ab26b6faf1679a1da06554fd32
hadongkyoun <gkehdrbs36@naver.com>
2022-10-02 09:11:26 +0900

    server1

--- node.js/createServer.js
+++ node.js/createServer.js
@@ -2,4 +2,10 @@ const http = require('http');
 
 http.createServer((req, res)=>{
 	//어떻게 응답하는 가에 대한 코드
-});
+	res.writeHead(200, {'Content-type': 'text/html; charset=utf-8'});
+	res.write('<h1>Hello Node!</h1>');
+	res.end('<p>Hello Server!</p>');
+})
+	.listen(8080, ()=>{
+		console.log('8080번 포트에서 서버 대기 중..');
+	});
